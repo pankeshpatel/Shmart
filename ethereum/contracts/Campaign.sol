d2ae927a351857f5485f2f61c9f6511fb673ab80
pankeshpatel <pankeshlinux@gmail.com>
2019-07-07 13:56:44 +0530

    added factory contract into the contract file

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.17;
 
+
+// This is to list the address of  all deployed contracts.
+// The deployed contract's cost will be charged to the Caller's
+// account. Because he has been calling this function.
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
@@ -12,6 +16,8 @@ contract CampaignFactory {
         return deployedCampaigns;
     }
 }
+
+// Each Campaign contract has its own address in the Blockchain.
 
 contract Campaign {
 
@@ -30,10 +36,15 @@ contract Campaign {
         //been processed (money sent)
         bool complete;
 
-
+        // Tracks number of "Yes" votes for this
+        // request.
         uint approvalCount;
 
         // this is for votting mechanism
+        // It tracks who has voted on a given request.
+
+        // Every Request in a campaign contract will
+        // have its own set of approvals.
         mapping(address => bool) approvals;
     }
 
@@ -49,9 +60,29 @@ contract Campaign {
     // a contributor or approver
     uint public minimumContribution;
 
-    // Lisy of addresses for every person
+    // List of addresses for every person
     // who has donated the money.
+
+    // mapping
+    // It is the collection of key value pairs
+    // All the keys must be of the same type, and
+    // all values must be of the same type.
+
+    // mapping
+    // It takes constant search time.
+    // This is because it has HASH index.
+    // It maps to the hashing values.
+
+    // address --
+    // the keys are "address" of people, who donates to
+    // the contract.
+
+    // bool --
+    // the values are boolean (True/ False).
+    // By default value is "False"
     mapping(address => bool) public approvers;
+
+
     uint public approversCount;
 
     modifier restricted() {
@@ -73,6 +104,8 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
+        // msg.sender is a key,
+        // "true" assignment is value.
         approvers[msg.sender] = true;
         approversCount++;
     }
@@ -82,16 +115,19 @@ contract Campaign {
     function createRequest(string description, uint value, address recipient) public restricted {
        // The following are Data holding places:
 
-       // Storage -- holds data between function calls
+       // 1. Storage -- holds data between function calls
        // Pretty much like a computer's hard drive.
        // The declared variables in this contract
        // is an example of storage.
 
-       // Memory -- Temporary place to store Data
+
+       // 2. Memory -- Temporary place to store Data
        // Pretty much like a computer's RAM.
        // the function ARGUMENT (e.g., minimumContribution)
        // is an example of "memory".
 
+
+       // We have been creating "newRequest" temporarily.
         Request memory newRequest = Request({
            description: description,
            value: value,
@@ -104,11 +140,30 @@ contract Campaign {
     }
 
     // Called by the each contributor to approver
-    // a spending request
+    // a spending request.
+
+    // Req1 :  A contributor cannot vote multiple time
+    // on a single request.
+
+    // Req2:  Resiliant to many many contributors.
+
+    // The goal of approveRequest() is to approve a specific
+    // request. Therefore, the caller of this function has
+    // to provide "index".
     function approveRequest(uint index) public {
+
+        // "requests" is an array. The following
+        // will access the index of the requests.
         Request storage request = requests[index];
 
+        // This would check the address of the contributor
+        // (who has called approveRequest(..)) exists in
+        // approvers[]
+
+        // Check if the approvers exists
         require(approvers[msg.sender]);
+
+        // check if the callers has not voted before.
         require(!request.approvals[msg.sender]);
 
         request.approvals[msg.sender] = true;
@@ -121,9 +176,17 @@ contract Campaign {
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
 
+        // Check that more than 50% of contributors
+        // have approved the request.
         require(request.approvalCount > (approversCount / 2));
+
+        // Check wheather the request is processed.
+        // If it is processed (True), then exist the function
+        // If is is not processed (False), then execute the function
+        // to the next line of this function.
         require(!request.complete);
 
+        // Transfer the value to the recipient.
         request.recipient.transfer(request.value);
         request.complete = true;
     }
