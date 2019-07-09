b412bb71b801f5a628507b11733b02ab258b73f7
pankeshpatel <pankeshlinux@gmail.com>
2019-07-09 12:46:47 +0530

    Revised web.js for client side rendering

--- .next/dist/ethereum/web3.js
+++ .next/dist/ethereum/web3.js
@@ -12,8 +12,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var web3 = void 0;
 
+// our server will send JavaScript code to the browser to execute.
+// If this code finds that metamask plugin is installed, then it will
+// use windows variable. Otherwise, it will make an HTTP request to Rinkby.
+
+
 // check that we are in the browser and check that metamask is running
 // This file configure web3 with a provider from metamask.
+
+// The overall motivation of this file is that
+// the users (who have been accessing our website)
+// do not need to install metamask browser plugin
+// in order to access our website.
+
+// Since our server is accessing the metamask
+// plugin, the users do not need to install the plugin
+
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running.
@@ -24,4 +38,4 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new _web2.default(provider);
 }
 exports.default = web3;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImV0aGVyZXVtXFx3ZWIzLmpzIl0sIm5hbWVzIjpbIldlYjMiLCJ3ZWIzIiwid2luZG93IiwiY3VycmVudFByb3ZpZGVyIiwicHJvdmlkZXIiLCJwcm92aWRlcnMiLCJIdHRwUHJvdmlkZXIiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUVBLEFBQU87Ozs7OztBQUVQLElBQUksWUFBSjs7QUFFQTtBQU5BOztBQU9BLElBQUksT0FBQSxBQUFPLFdBQVAsQUFBa0IsZUFBZSxPQUFPLE9BQVAsQUFBYyxTQUFuRCxBQUE0RCxhQUFhLEFBQ3ZFO0FBQ0E7U0FBTyxBQUFJLGtCQUFLLE9BQUEsQUFBTyxLQUF2QixBQUFPLEFBQXFCLEFBQzdCO0FBSEQsT0FHTyxBQUNMO0FBQ0E7TUFBTSxXQUFXLElBQUksY0FBQSxBQUFLLFVBQVQsQUFBbUIsYUFBcEMsQUFBaUIsQUFDZixBQUVGO1NBQU8sQUFBSSxrQkFBWCxBQUFPLEFBQVMsQUFDakI7QUFDRDtrQkFBQSxBQUFlIiwiZmlsZSI6IndlYjMuanMiLCJzb3VyY2VSb290IjoiRDovcmVhY3Qva2lja3N0YXJ0L1NobWFydCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImV0aGVyZXVtXFx3ZWIzLmpzIl0sIm5hbWVzIjpbIldlYjMiLCJ3ZWIzIiwid2luZG93IiwiY3VycmVudFByb3ZpZGVyIiwicHJvdmlkZXIiLCJwcm92aWRlcnMiLCJIdHRwUHJvdmlkZXIiXSwibWFwcGluZ3MiOiI7Ozs7OztBQVlBLEFBQU87Ozs7OztBQUVQLElBQUksWUFBSjs7QUFFQTtBQUNBO0FBQ0E7OztBQUlBO0FBdEJBOztBQUVBO0FBQ0E7QUFDQTtBQUNBOztBQUVBO0FBQ0E7OztBQWVBLElBQUksT0FBQSxBQUFPLFdBQVAsQUFBa0IsZUFBZSxPQUFPLE9BQVAsQUFBYyxTQUFuRCxBQUE0RCxhQUFhLEFBQ3ZFO0FBQ0E7U0FBTyxBQUFJLGtCQUFLLE9BQUEsQUFBTyxLQUF2QixBQUFPLEFBQXFCLEFBQzdCO0FBSEQsT0FHTyxBQUNMO0FBQ0E7TUFBTSxXQUFXLElBQUksY0FBQSxBQUFLLFVBQVQsQUFBbUIsYUFBcEMsQUFBaUIsQUFDZixBQUVGO1NBQU8sQUFBSSxrQkFBWCxBQUFPLEFBQVMsQUFDakI7QUFDRDtrQkFBQSxBQUFlIiwiZmlsZSI6IndlYjMuanMiLCJzb3VyY2VSb290IjoiRDovcmVhY3Qva2lja3N0YXJ0L1NobWFydCJ9
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,8 +1,24 @@
 // This file configure web3 with a provider from metamask.
 
+// The overall motivation of this file is that
+// the users (who have been accessing our website)
+// do not need to install metamask browser plugin
+// in order to access our website.
+
+// Since our server is accessing the metamask
+// plugin, the users do not need to install the plugin
+
+
+
 import Web3 from 'web3';
 
 let web3;
+
+// our server will send JavaScript code to the browser to execute.
+// If this code finds that metamask plugin is installed, then it will
+// use windows variable. Otherwise, it will make an HTTP request to Rinkby.
+
+
 
 // check that we are in the browser and check that metamask is running
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
