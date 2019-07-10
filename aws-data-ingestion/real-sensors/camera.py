039e12e800423b6ccdef385ba5278825571d68d1
pankeshpatel <pankeshlinux@gmail.com>
2019-07-10 21:31:17 +0530

    removed aws repo

--- aws-data-ingestion/real-sensors/camera.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-camera.start_preview()
-sleep(10)
-camera.capture('/home/pi/real-sensors/file-upload-s3/image.jpg')
-camera.stop_preview()
